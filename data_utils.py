9a7c37e3637c69a00c2b15e69fca4eecf7439800
alireza1989 <alireza.darbehani@gmail.com>
2018-09-10 17:32:40 -0400

    Finish input and houtput matrix data preparation

--- data_utils.py
+++ data_utils.py
@@ -1,4 +1,5 @@
-import pandas as pd 
+import pandas as pd
+import numpy as np
 
 
 
@@ -18,6 +19,7 @@ def readJson(path):
     df = pd.read_json(jsonFile, orient='columns')
 
     return df
+
 
 def dataDistributionMap(column):
     '''
@@ -54,3 +56,68 @@ def dataDistributionMap(column):
             else:
                 map[item] = 1
     return map
+
+
+def wordsToMap(wordsList):
+    '''
+    Function cretaes two maps of a list of words -  word to index and index to words
+
+    Arguments:
+    wordsList -- A python list of words
+
+    Returns:
+    w2i -- A python map of words to index
+    i2w -- A python map of index to corresponding word
+    '''
+
+    w2i = {val : index for index, val in enumerate(wordsList)}
+    i2w = {index : val for index, val in enumerate(wordsList)}
+    return w2i, i2w
+
+
+def convertToInputMatrix(recipes, wordToIndex):
+    '''
+    Function creates a large input matrix that has the recipes encoded -- each row is a recipe
+
+    Arguments:
+    recipes -- A list of all recipes
+    wordToIndex -- A python map that contains the index of each ingredients
+
+    Returns:
+    encodedRecipes -- A big numpy matrix that has all the rcipes encoded with zero padding
+    '''
+    # find the size of longest recipe in the list
+    longestRecipe = max(len(recipe) for recipe in recipes)
+
+
+    # make a matrix of zeros
+    encodedRecipes = np.zeros((recipes.shape[0], longestRecipe), dtype=int)
+    
+    for i in range(0, len(recipes)):
+        for j in range(0, len(recipes[i])):
+            encodedRecipes[i][j] = wordToIndex[recipes[i][j]]
+
+    return encodedRecipes
+
+
+def convertToOutputMatrix(cuisines, cuisineToIndex):
+    '''
+    Fucntion creates a matrix of expected out put lables (cuisine types)
+
+    Arguments:
+    cuisines -- An ordered list of cuisines in the training set
+
+    cuisineToIndex -- A map of cuisine to an index
+
+    Returns:
+    cuisine1H -- A one hot embedding of cuisines type associates with each recipe
+    '''
+
+    # create the output matrix with zeros
+    cuisine1H = np.zeros((len(cuisines), len(cuisineToIndex)), dtype=int)
+
+    
+    for i in range(0, len(cuisines)):
+        cuisine1H[i][cuisineToIndex[cuisines[i]]] =  1
+
+    return cuisine1H
